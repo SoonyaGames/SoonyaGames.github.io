223965af2af7f134501c68680fa4ad319bc0f28c
dissmas <dissmas@users.noreply.github.com>
2018-05-03 20:15:38 +0300

    На главную страницу добавлены часы

--- DigitalClock.js
+++ DigitalClock.js
@@ -4,19 +4,19 @@
 		<title> Digital Clock </title>
 		<style type= "text/css">
 			body
-            {
-			  background: #FFF; 
-            }      
-			.clock
-			{
-		      position: absolute;
-			  top: 5%;
-			  left: 87%;
-			  color: #000;
-			  font-size: 30px;
-			  border: 1px solid #fff;
-			  padding: 0px 5px 0px 5px;
+			  {
+			    background: #00BFFF; 
+			  }
+			.clock{
+		            position absolute;
+			    top: 50%;
+			    left: 50%;
+			    color: #fff;
+			    front-size: 80px;
+			    padding: 0px 5px 0px 5px;
 			}
+			    
+			
 		</style>
 	</head>
 
@@ -25,10 +25,10 @@
 		<script type="text/javascript">
 		function showTime()
 		{
-            var date = new Date();
-            var h = date.getHours(); // 0 - 23
-            var m = date.getMinutes(); // 0 - 59
-            var s = date.getSeconds(); // 0 - 59
+    			var date = new Date();
+    			var h = date.getHours(); // 0 - 23
+    			var m = date.getMinutes(); // 0 - 59
+    			var s = date.getSeconds(); // 0 - 59
     			
 			h = (h<10)?"0"+h:h;
 			m = (m<10)?"0"+m:m;
